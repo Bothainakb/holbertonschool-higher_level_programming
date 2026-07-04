984cfa98281ee2f2125c03f2d4b2e3c11da49c73
bothainakb <bothainakb@gmail.com>
2026-07-04 21:28:47 +0200

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,8 @@
 
 
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return the dictionary description of an object.
+
+    Used for JSON serialization.
+    """
     return obj.__dict__
