1bbed2241de9d707036a33dbc393007c771ce801
bothainakb <bothainakb@gmail.com>
2026-08-13 11:21:33 +0000

    3-script.js

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('header');
+
+document.querySelector('#toggle_header').addEventListener('click', () => {
+  if (header.classList.contains('red')) {
+    header.classList.remove('red');
+    header.classList.add('green');
+  } else {
+    header.classList.remove('green');
+    header.classList.add('red');
+  }
+});
